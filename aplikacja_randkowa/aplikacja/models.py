9cba7a291e9a34b4463cde4cb6e3ba32f3152b39
MilenaChibowska <mchibowska@st.swps.edu.pl>
2025-11-07 13:08:46 +0100

    dodanie admin

--- aplikacja_randkowa/aplikacja/models.py
+++ aplikacja_randkowa/aplikacja/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
+PLEC_WYBOR = (
+    ("K", "kobieta"),
+    ("M", "mezczyzna"),
+    ("I", "inna")
+)
+
 class Osoba(models.Model):
-    PLEC_WYBOR = (
-        ("K", "kobieta"),
-        ("M", "mezczyzna"),
-        ("I", "inna")
-    )
+
     imie = models.CharField(max_length=50, null = False, blank = False)
     nazwisko = models.CharField(max_length=100, null = False, blank = False)
     plec = models.CharField(max_length=1, choices= PLEC_WYBOR, default= "I")
