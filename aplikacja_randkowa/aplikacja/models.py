fe2a54037f2c7c6511acae11a3f57e69b02e8360
MilenaChibowska <mchibowska@st.swps.edu.pl>
2025-11-07 12:54:15 +0100

    Zmiana modelu

--- aplikacja_randkowa/aplikacja/models.py
+++ aplikacja_randkowa/aplikacja/models.py
@@ -1,5 +1,12 @@
 from django.db import models
-class Person(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+
+class Osoba(models.Model):
+    PLEC_WYBOR = (
+        ("K", "kobieta"),
+        ("M", "mezczyzna"),
+        ("I", "inna")
+    )
+    imie = models.CharField(max_length=50, null = False, blank = False)
+    nazwisko = models.CharField(max_length=100, null = False, blank = False)
+    plec = models.CharField(max_length=1, choices= PLEC_WYBOR, default= "I")
     
