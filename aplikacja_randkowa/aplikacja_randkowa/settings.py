569ffc0fbfe045b54daab626e10c2387e0d56878
MilenaChibowska <mchibowska@st.swps.edu.pl>
2025-12-12 13:36:13 +0100

    dodanie settings

--- aplikacja_randkowa/aplikacja_randkowa/settings.py
+++ aplikacja_randkowa/aplikacja_randkowa/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DATETIME_FORMAT': "%Y-%m-%dT%H:%M:%S%z",
+}
